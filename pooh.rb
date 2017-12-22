6946e9fe8a7c42be52670fda9535c95141330512
Jacob Rockland <jakerockland@gmail.com>
2017-12-22 00:03:37 -0700

    updated url to raw url

--- pooh.rb
+++ pooh.rb
@@ -1,7 +1,7 @@
 class Pooh < Formula
   desc "A simple program for adding one of Github's useful .gitignore templates to your project"
   homepage "https://github.com/jakerockland/pooh"
-  url "https://github.com/jakerockland/pooh/blob/master/pooh.rb"
+  url "https://raw.githubusercontent.com/jakerockland/pooh/master/pooh.rb"
   sha256 "1b41a948e36bb62e7187b6224aac770d219e6cebf56e1ae9b1d6dec37f59163e"
   version "0.1.0"
 
