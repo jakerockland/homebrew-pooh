0f4e446d286164a03706cb48a0711068fcb77ccc
Jacob Rockland <jakerockland@gmail.com>
2017-12-21 23:58:06 -0700

    initial homebrew formula

---
+++ pooh.rb
@@ -0,0 +1,13 @@
+class Pooh < Formula
+  desc "A simple program for adding one of Github's useful .gitignore templates to your project
+  homepage "https://github.com/jakerockland/pooh"
+  url "https://github.com/jakerockland/pooh/blob/master/pooh.rb"
+  sha256 "1b41a948e36bb62e7187b6224aac770d219e6cebf56e1ae9b1d6dec37f59163e"
+  version "0.1.0"
+
+  depends_on "ruby"
+
+  def install
+    bin.install "pooh"
+  end
+end
