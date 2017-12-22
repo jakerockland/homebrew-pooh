5be0afdd372ac90fbc4b4c6c6d8e6664f14fac92
Jacob Rockland <jakerockland@gmail.com>
2017-12-22 00:10:43 -0700

    updated url to tar and shasum

--- pooh.rb
+++ pooh.rb
@@ -1,8 +1,8 @@
 class Pooh < Formula
   desc "A simple program for adding one of Github's useful .gitignore templates to your project"
   homepage "https://github.com/jakerockland/pooh"
-  url "https://raw.githubusercontent.com/jakerockland/pooh/master/pooh.rb"
-  sha256 "a26b1a367e83f406ce3cc5fb755393731f82210a04f590859bf14897127a20c7"
+  url "https://github.com/jakerockland/pooh/archive/v0.1.0.tar.gz"
+  sha256 "5208cbb7362efc12a545b77a1471ee1473c4dc938676d3b10be140ef32b927f0"
   version "0.1.0"
 
   depends_on "ruby"
