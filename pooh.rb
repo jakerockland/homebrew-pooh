d2f65ac4281e9043b96b2c3cb66b066c027ebfab
Jacob Rockland <jakerockland@gmail.com>
2017-12-22 00:26:42 -0700

    updated checksum

--- pooh.rb
+++ pooh.rb
@@ -2,7 +2,7 @@ class Pooh < Formula
   desc "A simple program for adding one of Github's useful .gitignore templates to your project"
   homepage "https://github.com/jakerockland/pooh"
   url "https://github.com/jakerockland/pooh/archive/v0.1.0-alpha.tar.gz"
-  sha256 "5208cbb7362efc12a545b77a1471ee1473c4dc938676d3b10be140ef32b927f0"
+  sha256 "108a446672b8bf86c217e48b160fea343f0e017eca8216933dbcc7b3326d1c03"
   version "0.1.0"
 
   depends_on "ruby"
